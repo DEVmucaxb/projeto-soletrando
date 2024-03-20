470dab87dd19d5ba7b55d162114d97bc1ebc76e1
Samuel Felipe <155489331+DEVmucaxb@users.noreply.github.com>
2024-03-20 19:51:51 -0300

    concluí o banco de (120) palavras

--- assets/scripts/questions.js
+++ assets/scripts/questions.js
@@ -38,5 +38,489 @@ let spell = [
     {
         word: 'fraseador',
         id: 9
+    },
+    {
+        word: 'trejeito',
+        id: 10
+    },
+    {
+        word: 'hipnose',
+        id: 11
+    },
+    {
+        word: 'pugilista',
+        id: 12
+    },
+    {
+        word: 'conciso',
+        id: 13
+    },
+    {
+        word: 'ojeriza',
+        id: 14
+    },
+    {
+        word: 'democratizar',
+        id: 15
+    },
+    {
+        word: 'transubstanciação',
+        id: 16
+    },
+    {
+        word: 'resiliência',
+        id: 17
+    },
+    {
+        word: 'execução',
+        id: 18
+    },
+    {
+        word: 'capcioso',
+        id: 19
+    },
+    {
+        word: 'excursão',
+        id: 20
+    },
+    {
+        word: 'exceção',
+        id: 21
+    },
+    {
+        word: 'abrasivo',
+        id: 22
+    },
+    {
+        word: 'extraterrestre',
+        id: 23
+    },
+    {
+        word: 'chimarrão',
+        id: 24
+    },
+    {
+        word: 'dizímo',
+        id: 25
+    },
+    {
+        word: 'imprescindível',
+        id: 26
+    },
+    {
+        word: 'dicionário',
+        id: 27
+    },
+    {
+        word: 'recíproco',
+        id: 28
+    },
+    {
+        word: 'cônjuge',
+        id: 29
+    },
+    {
+        word: 'anuência',
+        id: 30
+    },
+    {
+        word: 'diligência',
+        id: 31
+    },
+    {
+        word: 'cínico',
+        id: 32
+    },
+    {
+        word: 'efêmero',
+        id: 33
+    },
+    {
+        word: 'intempérie',
+        id: 34
+    },
+    {
+        word: 'condolências',
+        id: 35
+    },
+    {
+        word: 'idôneo',
+        id: 36
+    },
+    {
+        word: 'contemporâneo',
+        id: 37
+    },
+    {
+        word: 'escrúpulo',
+        id: 38
+    },
+    {
+        word: 'supérfluo',
+        id: 39
+    },
+    {
+        word: 'propósito',
+        id: 40
+    },
+    {
+        word: 'parcimônia',
+        id: 41
+    },
+    {
+        word: 'essência',
+        id: 42
+    },
+    {
+        word: 'hipócrita',
+        id: 43
+    },
+    {
+        word: 'genuíno',
+        id: 44
+    },
+    {
+        word: 'parcimônia',
+        id: 45
+    },
+    {
+        word: 'misógino',
+        id: 46
+    },
+    {
+        word: 'implícito',
+        id: 47
+    },
+    {
+        word: 'índole',
+        id: 48
+    },
+    {
+        word: 'corolário',
+        id: 49
+    },
+    {
+        word: 'complacência',
+        id: 50
+    },
+    {
+        word: 'pragmático',
+        id: 51
+    },
+    {
+        word: 'inexorável',
+        id: 52
+    },
+    {
+        word: 'concupiscência',
+        id: 53
+    },
+    {
+        word: 'pêsames',
+        id: 54
+    },
+    {
+        word: 'jactância',
+        id: 55
+    },
+    {
+        word: 'carência',
+        id: 56
+    },
+    {
+        word: 'convergência',
+        id: 57
+    },
+    {
+        word: 'romanticamente',
+        id: 58
+    },
+    {
+        word: 'incógnita',
+        id: 59
+    },
+    {
+        word: 'leniência',
+        id: 60
+    },
+    {
+        word: 'recíproca',
+        id: 61
+    },
+    {
+        word: 'límpido',
+        id: 62
+    },
+    {
+        word: 'provérbio',
+        id: 63
+    },
+    {
+        word: 'custódia',
+        id: 64
+    },
+    {
+        word: 'sórdido',
+        id: 65
+    },
+    {
+        word: 'incólume',
+        id: 66
+    },
+    {
+        word: 'privilégio',
+        id: 67
+    },
+    {
+        word: 'verossímil',
+        id: 68
+    },
+    {
+        word: 'pretérito',
+        id: 69
+    },
+    {
+        word: 'relevância',
+        id: 70
+    },
+    {
+        word: 'sufrágio',
+        id: 71
+    },
+    {
+        word: 'importância',
+        id: 72
+    },
+    {
+        word: 'coincidência',
+        id: 73
+    },
+    {
+        word: 'heterogêneo',
+        id: 74
+    },
+    {
+        word: 'prescindível',
+        id: 75
+    },
+    {
+        word: 'íntegro',
+        id: 76
+    },
+    {
+        word: 'indivíduo',
+        id: 77
+    },
+    {
+        word: 'controvérsia',
+        id: 78
+    },
+    {
+        word: 'coerência',
+        id: 79
+    },
+    {
+        word: 'orgânico',
+        id: 80
+    },
+    {
+        word: 'militância',
+        id: 81
+    },
+    {
+        word: 'parâmetro',
+        id: 82
+    },
+    {
+        word: 'intercorrência',
+        id: 83
+    },
+    {
+        word: 'miserável',
+        id: 84
+    },
+    {
+        word: 'princípio',
+        id: 85
+    },
+    {
+        word: 'possível',
+        id: 86
+    },
+    {
+        word: 'assintomático',
+        id: 87
+    },
+    {
+        word: 'consciência',
+        id: 88
+    },
+    {
+        word: 'vovó',
+        id: 89
+    },
+    {
+        word: 'abominável',
+        id: 90
+    },
+    {
+        word: 'ridículo',
+        id: 91
+    },
+    {
+        word: 'providência',
+        id: 92
+    },
+    {
+        word: 'ausência',
+        id: 93
+    },
+    {
+        word: 'ninfomaníaco',
+        id: 94
+    },
+    {
+        word: 'comprobatório',
+        id: 95
+    },
+    {
+        word: 'irrepreensível',
+        id: 96
+    },
+    {
+        word: 'incongruência',
+        id: 97
+    },
+    {
+        word: 'conferência',
+        id: 98
+    },
+    {
+        word: 'nômade',
+        id: 99
+    },
+    {
+        word: 'antagônico',
+        id: 90
+    },
+    {
+        word: 'reminiscência',
+        id: 91
+    },
+    {
+        word: 'sistemático',
+        id: 92
+    },
+    {
+        word: 'província',
+        id: 93
+    },
+    {
+        word: 'hipótese',
+        id: 94
+    },
+    {
+        word: 'solidário',
+        id: 95
+    },
+    {
+        word: 'instável',
+        id: 96
+    },
+    {
+        word: 'moléstia',
+        id: 97
+    },
+    {
+        word: 'ápice',
+        id: 98
+    },
+    {
+        word: 'maledicência',
+        id: 99
+    },
+    {
+        word: 'extemporâneo',
+        id: 100
+    },
+    {
+        word: 'intolerância',
+        id: 101
+    },
+    {
+        word: 'formidável',
+        id: 102
+    },
+    {
+        word: 'ânsia',
+        id: 103
+    },
+    {
+        word: 'melancólico',
+        id: 104
+    },
+    {
+        word: 'factível',
+        id: 105
+    },
+    {
+        word: 'implacável',
+        id: 106
+    },
+    {
+        word: 'flâmula',
+        id: 107
+    },
+    {
+        word: 'lunático',
+        id: 108
+    },
+    {
+        word: 'locatário',
+        id: 109
+    },
+    {
+        word: 'bíblia',
+        id: 110
+    },
+    {
+        word: 'competência',
+        id: 111
+    },
+    {
+        word: 'desprezível',
+        id: 112
+    },
+    {
+        word: 'perecível',
+        id: 113
+    },
+    {
+        word: 'simplório',
+        id: 114
+    },
+    {
+        word: 'longânimo',
+        id: 115
+    },
+    {
+        word: 'petulância',
+        id: 116
+    },
+    {
+        word: 'mercê',
+        id: 117
+    },
+    {
+        word: 'aquiescência',
+        id: 118
+    },
+    {
+        word: 'assiduidade',
+        id: 119
+    },
+    {
+        word: 'miçanga',
+        id: 120
     }
 ];
